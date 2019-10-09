cd91ac28c744a2159421149ff506ed2b4938d11a
Alfons0329 <alfons.cs04@g2.nctu.edu.tw>
2019-10-09 17:43:38 +0800

    Finished code and writeup report for HW0x02

--- Hw0x02/solve.py
+++ Hw0x02/solve.py
@@ -3,8 +3,19 @@ flag_encoded = '\x0f\x09\x31\x0c\xf8\x14\xed\x36\xfa\xee\xe2\xed\x36\x1e\x36\x0c
 flag_encoded2 = '\x0F\x09\x02\x0C\xF8\xFA\x30\xF0\x22\x22\xFA\x30\xF0\x22\x22\xFA\x30\xF0\x22\x22\x35\xED\xE4\xF6\xFA\xE4\xEC\x35\xE1\x22\x22\xC6' # real flag in debugger line from 0x750000 to 0x75004d
 
 flag_final = ''
-for i in flag_encoded2:
+for i in flag_encoded:
     flag_final += chr((ord(str(i)) ^ 0x66) - 0x23)
 
 print(16)
 print(flag_final)
+
+'''
+# test
+flag = 'FLAG{'
+hex_str = []
+
+for i in flag:
+    hex_str.append(hex((ord(i) + 0x23) ^ 0x66))
+
+print(hex_str)
+'''
