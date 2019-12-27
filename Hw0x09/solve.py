cd896d93bc8d56716799f96b79bc575b17767cd4
Alfons0329 <alfons.cs04@g2.nctu.edu.tw>
2019-12-27 21:24:58 +0800

    Doing Hw0x09, CatHub Party (cryptography)

---
+++ Hw0x09/solve.py
@@ -0,0 +1,20 @@
+import requests, urllib3
+import base64
+
+cipher_b64 = '2hvi8C%2Buwz9w77fyC9IAMyWrv9VHSg5SFpmbxw6lx%2Bqxe3pCelcFob6C2kMUZBW6Gk9x1j%2B%2Btw%2FBCkcLBCcSBkY5TCW6t4NX0L7f%2FKUvMXUYt%2FBzbQ15AcsjxvlvfDrI'
+cipher = ''
+phpsessid = '8ll7jijdl3mr0mrrj4hlpss690'
+
+def interact_with_site(session, guessed_cipher):
+    cookie = {
+        'PHPSESSID': phpsessid, 
+        'FLAG':
+    }
+    return status
+
+
+def main():
+    return 0
+
+
+main()
