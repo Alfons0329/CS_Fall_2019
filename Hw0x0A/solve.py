cbf277e7c0b65286702c28f95fb58ef3d3422c97
Alfons0329 <alfons.cs04@g2.nctu.edu.tw>
2020-01-05 19:57:09 +0800

    Finished Hw0x0A, writeup after 1/7

--- Hw0x0A/solve.py
+++ Hw0x0A/solve.py
@@ -3,66 +3,70 @@
 from pwn import *
 from Crypto.Util.number import *
 import math
+import sys, os
 
-r = remote('edu-ctf.csie.org', 10192)
+rem = remote('edu-ctf.csie.org', 10192)
 MOD = 16
 
 # use pollard p - 1 for RSA forging
-r.sendlineafter('>', '1')
-c = r.recvline().split()[-1]
-e = r.recvline().split()[-1]
-n = r.recvline().split()[-1]
-print('received c ', c, ' e ', e, ' n ', n)
-
-# some required type conversion
+rem.sendlineafter('>', '1')
+c = rem.recvline().split()[-1]
+e = rem.recvline().split()[-1]
+n = rem.recvline().split()[-1]
 c = int(c)
 e = int(e)
 n = int(n)
+print('received c ', c, ' e ', e, ' n ', n)
 
 # the flag will be (xn, xn - 1 ......, x1, x0)
 # with c -> m, and mod being 16
-lsb = 0
 
 #          ((base ^ -i) ^ (e)) * c ---> ((base ^ -i) ^) * m
 #  that is multiplied_c    -- oracle -> multiplied_m
 
-base = inverse(MOD, c)
-pow_cnt = 0
+base = inverse(MOD, n)
 continuous_padding = 0
-flag_list = []
 
 # init r for modular congruence
 r = 0
+pow_cnt = 0
+try_cnt = 0
+flag_res = 0
+flag_list = []
 
-while True:
-    r.sendlineafter('>', '2')
+while try_cnt < 1024 // 4:
+    rem.sendlineafter('>', '2')
 
     # under the mod n cycle
     multiplied_c = pow(base, pow_cnt * e, n) * c % n
-    r.sendline(str(multiplied_c))
-    multiplied_m = int(r.recvline().split()[-1])
-    print('multiplied_m --> ', multiplied_m)
+    rem.sendline(str(multiplied_c))
+    multiplied_m = int(rem.recvline().split()[-1])
+    r = multiplied_m % n % MOD
+
+    tmp = 0
+    start_pow = len(flag_list)
+    for i in range(len(flag_list)):
+        tmp += pow(base, start_pow, n) * flag_list[i]
+        start_pow -= 1
+
+    xi = (r - ((tmp) % n)) % MOD
 
     # check if the received bit(ot byte) is the padding zero (continuously padding zero), break as the threshold meets
-    if multiplied_m == 0:
-        if continuous_padding >= 20:
+    if xi == 0:
+        if continuous_padding >= 10:
             break
         else:
             continuous_padding += 1
     else:
         continuous_padding = 0
-
-    r = multiplied_m % n % MOD
-
-    tmp = 0
-    start_pow = -len(flag_list)
-    for i in range(len(flag_list)):
-        tmp += pow(base, start_pow, n)
-        start_pow += 1
-
-    xi = r - ((tmp) % n) % MOD
     flag_list.append(xi)
 
-print('FLAG --> ')
-for i in flag_list:
-    print(chr(i), endline='')
+    # make ((base ^ -i) ^) * m --> m
+    flag_res = (((MOD ** pow_cnt) % n * xi) %n + flag_res) % n
+    pow_cnt += 1
+    try_cnt += 1
+
+print(flag_res)
+flag_res = bytes.fromhex(str(hex(flag_res))[2:])
+for i in flag_res:
+    print(chr(int(i)), end='')
