df47d08e9114e194ed05522d36271086b03b3028
Alfons0329 <alfons.cs04@g2.nctu.edu.tw>
2020-01-05 17:46:07 +0800

    Doing Hw0x0A, RSA LSB Attack

--- Hw0x0A/solve.py
+++ Hw0x0A/solve.py
@@ -19,7 +19,7 @@ c = int(c)
 e = int(e)
 n = int(n)
 
-# the flag will be (a0, a1, ... an)
+# the flag will be (xn, xn - 1 ......, x1, x0)
 # with c -> m, and mod being 16
 lsb = 0
 
@@ -28,6 +28,11 @@ lsb = 0
 
 base = inverse(MOD, c)
 pow_cnt = 0
+continuous_padding = 0
+flag_list = []
+
+# init r for modular congruence
+r = 0
 
 while True:
     r.sendlineafter('>', '2')
@@ -37,3 +42,27 @@ while True:
     r.sendline(str(multiplied_c))
     multiplied_m = int(r.recvline().split()[-1])
     print('multiplied_m --> ', multiplied_m)
+
+    # check if the received bit(ot byte) is the padding zero (continuously padding zero), break as the threshold meets
+    if multiplied_m == 0:
+        if continuous_padding >= 20:
+            break
+        else:
+            continuous_padding += 1
+    else:
+        continuous_padding = 0
+
+    r = multiplied_m % n % MOD
+
+    tmp = 0
+    start_pow = -len(flag_list)
+    for i in range(len(flag_list)):
+        tmp += pow(base, start_pow, n)
+        start_pow += 1
+
+    xi = r - ((tmp) % n) % MOD
+    flag_list.append(xi)
+
+print('FLAG --> ')
+for i in flag_list:
+    print(chr(i), endline='')
