e04ee3a20e8c60cb742325b94dd6170ec275a749
Alfons0329 <alfons.cs04@g2.nctu.edu.tw>
2020-01-11 22:40:01 +0800

    Doing AIS3 EOF CTF Crypto/rsactr

--- Final_CTF/rsactr/solve.py
+++ Final_CTF/rsactr/solve.py
@@ -3,21 +3,26 @@ from pwn import *
 import base64, sys, os, binascii
 import struct
 
-# rem = remote('', 20000)
+# rem = remote('eductf.zoolab.org', 20000)
 rem = process('./server.py', 20000)
 rem.sendlineafter('>', '1')
+
+# get public key pair, (n, e)
 n = rem.recvline(keepends=False).split()[-1]
 e = rem.recvline(keepends=False).split()[-1]
 
+# get c, the cipher text wanna attacking
 rem.sendlineafter('>', '2')
 c = rem.recvline(keepends=False).split()[-1]
 
+# y = c * x ^ e mod n
 n = int(n)
 e = int(e)
-c = int.from_bytes(c, 'little')
-
+c = int(c, 16)
 x = 2
-y = (c * (x ** e)) % n
+mul = pow(x, e, n)
+y = c * mul % n
+y = inverse(y, n) % n
 
 # y = bytes.fromhex(str(hex(y))[2:])
 rem.sendlineafter('>', '3')
@@ -29,4 +34,4 @@ x_inv = inverse(x, n)
 p = (z * x_inv) % n
 p = bytes.fromhex(str(hex(p))[2:])
 for i in p:
-        print(chr(int(i)), end='')
+    print(chr(int(i)), end='')
